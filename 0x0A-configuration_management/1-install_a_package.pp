bb70ff2531a589e76193a3de8a18b7bc5afa2514
ITSNE01 <rabie.haddar1@gmail.com>
2025-01-10 09:03:33 -0800

    1-install_a_package.pp fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
 # Using Puppet, install flask 2.1.0 from pip3
 
-package { 'flask':
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { 'Warkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
