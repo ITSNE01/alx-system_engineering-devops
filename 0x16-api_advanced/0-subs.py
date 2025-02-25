37b98f93ef5ccbbfa5b20a1cae98feb90393897b
ITSNE01 <rabie.haddar1@gmail.com>
2025-02-25 04:36:04 -0800

    fix 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Return the total number of subscribers on a given subreddit """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    response = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "MyPythonScript"})
+    if response.status_code >= 300:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    return response.json().get("data").get("subscribers")
