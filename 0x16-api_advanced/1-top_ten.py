d9b64acee1aeabb8911be216786505be1165a587
ITSNE01 <rabie.haddar1@gmail.com>
2025-02-25 04:38:26 -0800

    fix 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,17 +6,12 @@ import requests
 
 def top_ten(subreddit):
     """ Prints the titles of the 10 hottest posts on a given subreddit """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
+    response = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "MyPythonScript"},
                             allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    if response.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in response.json().get("data").get("children")]
