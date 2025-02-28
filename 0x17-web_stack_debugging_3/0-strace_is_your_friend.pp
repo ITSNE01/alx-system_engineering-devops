0c1a1781ae723dbaafac0db09d6216503f2e66d2
ITSNE01 <rabie.haddar1@gmail.com>
2025-02-28 09:54:35 -0800

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,2 @@
 # fixes http 500 error in wp-settings.php
-
-exec { 'fix-wpsite_error':
-   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-   path    => '/usr/bin/:/bin/',
-}
+exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
